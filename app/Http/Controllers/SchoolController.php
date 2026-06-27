0a27a6d83734212984c9c8078586cfd8465cdca1
Umeshww <kashishverm1910@gmail.com>
2026-06-27 15:58:31 +0530

    sdsd

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -4,29 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Models\School;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SchoolController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function school_reg()
     {
         return view('school-reg');
     }
     public function school_regs(Request $request)
     {
-        School::create([
-            'school_name'=>$request->input('school_name'),
-            'school_email'=>$request->input('school_email'),
-            'phone'=>$request->input('phone'),
-            'address'=>$request->input('address')
-
-        ]);
-        $data = ['message' => 'School Registration Success', 'status' => 'success'];
-        return response()->json($data);
+        $data = DB::table('school')->where('school_email', $request->input('school_email'))->first();
+        if (!$data) {
+            School::create([
+                'school_name' => $request->input('school_name'),
+                'school_email' => $request->input('school_email'),
+                'phone' => $request->input('phone'),
+                'address' => $request->input('address')
+            ]);
+            $msg = ['message' => 'School Registration Success!', 'status' => 'success'];
+        } else {
+            $msg = ['message' => 'School Already Registered!', 'status' => 'success'];
+        }
+        return response()->json($msg);
     }
-    
-
-    
 }
