463037f826f202b04858f507b8f178c83c83b578
Umeshww <kashishverm1910@gmail.com>
2026-07-10 16:14:03 +0530

    GG

--- app/Http/Controllers/UserPushController.php
+++ app/Http/Controllers/UserPushController.php
@@ -137,7 +137,7 @@ class UserPushController extends Controller
     {
         $school_email = Crypt::decryptString($request->shq);
         $st_email = $request->sq;
-        $data = DB::table('notification')->where('school_email', $school_email)->where('user_email', $st_email)->orderBy('id', 'asc')->get();
+        $data = DB::table('notification')->where('school_email', $school_email)->where('user_email', $st_email)->get();
         Notification::where('school_email', $school_email)->where('user_email', $st_email)->update([
             'checks'=>1
         ]);
