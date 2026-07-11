0c502eeb4b49c8720f795e048e2cb33ea583b00b
Umeshww <kashishverm1910@gmail.com>
2026-07-11 14:53:50 +0530

    GG

--- app/Http/Controllers/ApkController.php
+++ app/Http/Controllers/ApkController.php
@@ -10,7 +10,7 @@ use Illuminate\Routing\Controller;
 class ApkController extends Controller
 {
     public function apk(Request $request)
-     {
+    {
         return view('apk-upload');
     }
     public function apk_upload(Request $request)
@@ -18,21 +18,17 @@ class ApkController extends Controller
         $file = $request->file('file');
         $file_name = $request->input('file_name');
 
-        if(!$file){
-            return response()->json(['success'=>'No File Found']);
-
+        if (!$file) {
+            return response()->json(['success' => 'No File Found']);
         }
 
-        if($file->getClientOriginalExtension()!="apk"){
-            return response()->json(['success'=>'only APK file Support ! ','filename'=>"❌". $file->getClientOriginalExtension()]);
+        if ($file->getClientOriginalExtension() != "apk") {
+            return response()->json(['success' => 'only APK file Support ! ', 'filename' => "❌" . $file->getClientOriginalExtension()]);
         }
-        $name = 'dddd.'.$file->getClientOriginalExtension();
-        $destination = public_path('downloads/');
-        $full_path = $destination.'/'.$name;
-        if(file_exists($full_path)){
-            unlink($full_path);
-        }
-        $file->move(public_path($destination, $name));
-        return response()->json(['success'=>'file upload successfully ','filename'=>$name]);
+        $name = 'dddd.' . $file->getClientOriginalExtension();
+        
+        $full_path = $file->storeAs('public/downloads', $name);
+
+        return response()->json(['success' => 'file upload successfully ', 'filename' => $full_path]);
     }
 }
